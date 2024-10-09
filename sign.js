1afb607e81e3ead83b8c0333939b7d6edee832e1
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2024-10-09 16:41:10 +0700

    add 6 file

---
+++ sign.js
@@ -0,0 +1,11 @@
+let value=+prompt('Type a number',0);
+switch(true){
+    case (value>0):
+        alert(1);
+        break;
+        case (value<0):
+            alert(-1);
+            break;
+            default:
+                alert(0);
+}
